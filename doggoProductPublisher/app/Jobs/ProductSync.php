9ca36e7209619620edced3859fb06be8d02db68b
Andrew Kerton <kerton.andrew@gmail.com>
2019-12-04 18:14:14 +1100

    Queue and job setup for product sync

--- doggoProductPublisher/app/Jobs/ProductSync.php
+++ doggoProductPublisher/app/Jobs/ProductSync.php
@@ -8,18 +8,25 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 
+use App\Utils\DogApiHelper;
+use App\Utils\ShopifyApiHelper;
+
 class ProductSync implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $dogApiHelper;
+    protected $shopifyApiHelper;
 
     /**
      * Create a new job instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(ShopifyApiHelper $shopifyApiHelper, DogApiHelper $dogApiHelper)
     {
-        //
+        $this->shopifyApiHelper = $shopifyApiHelper;
+        $this->dogApiHelper = $dogApiHelper;
     }
 
     /**
@@ -29,6 +36,9 @@ class ProductSync implements ShouldQueue
      */
     public function handle()
     {
-        //
+        $dogs = $this->dogApiHelper->extractAllAndStore();
+
+
+
     }
 }
